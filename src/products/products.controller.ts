7bfff25269afdc68018132ff2b2bad562a02e120
jessieward <jessica_ward25@milton.edu>
2025-02-12 22:22:23 -0500

    homework

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,10 +1,23 @@
-import { Controller, Post } from "../../node_modules/@nestjs/common";
-
+import { Controller, Body, Post } from '@nestjs/common';
+import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
+    constructor(private readonly productsService: ProductsService) { }
 
     @Post()
-    addProduct() : any 
+    addProduct(
+        @Body('title') prodTitle: string,
+        @Body('description') prodDesc: string,
+        @Body('price') prodPrice: number,
+    ) {
+        const generatedId = this.productsService.insertProduct(
+            prodTitle,
+            prodDesc,
+            prodPrice
+        );
+        return { id: generatedId };
+    }
+
 }
 
